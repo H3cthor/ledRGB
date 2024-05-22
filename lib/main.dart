4112f09c4d0b561f3393fb86f326e3ac42dc8814
Hector Saldaña <hectorsalbe@hotmail.com>
2024-05-22 17:25:01 -0600

    add led screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,131 @@
+import 'package:colortest/device.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 // import 'package:colortest/color_picker_screen.dart';
-import 'package:colortest/main_page.dart';
+// import 'package:colortest/main_page.dart';
 
-void main() async {
-  runApp(const MyApp());
+void main() {
+  runApp(const FlutterBlueApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FlutterBlueApp extends StatelessWidget {
+  const FlutterBlueApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bluethoot App',
-      theme: ThemeData(
-        useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // home: const ColorPickerScreen(),
-      home: const MainPage(),
+      color: Colors.lightBlue,
+      home: StreamBuilder<BluetoothState>(
+          stream: FlutterBlue.instance.state,
+          initialData: BluetoothState.unknown,
+          builder: (c, snapshot) {
+            final state = snapshot.data;
+            if (state == BluetoothState.on) {
+              return const FindDevicesScreen();
+            }
+            return BluetoothOffScreen(state: state);
+          }),
     );
   }
 }
+
+class BluetoothOffScreen extends StatelessWidget {
+  const BluetoothOffScreen({Key? key, this.state}) : super(key: key);
+
+  final BluetoothState? state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Icon(
+              Icons.bluetooth_disabled,
+              size: 200.0,
+              color: Colors.white54,
+            ),
+            Text(
+              'Bluetooth Adapter is ${state != null ? state.toString().substring(15) : 'not available'}.',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FindDevicesScreen extends StatelessWidget {
+  const FindDevicesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Find Devices'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            StreamBuilder<List<ScanResult>>(
+              stream: FlutterBlue.instance.scanResults,
+              initialData: const [],
+              builder: (c, snapshot) => Column(
+                children: snapshot.data!
+                    .map((result) => ListTile(
+                          title: Text(result.device.name == "" ? "No Name " : result.device.name),
+                          subtitle: Text(result.device.id.toString()),
+                          onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                            result.device.connect();
+                            return DeviceScreen(device: result.device);
+                          })),
+                        ))
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: StreamBuilder<bool>(
+        stream: FlutterBlue.instance.isScanning,
+        initialData: false,
+        builder: (c, snapshot) {
+          if (snapshot.data!) {
+            return FloatingActionButton(
+              onPressed: () => FlutterBlue.instance.stopScan(),
+              backgroundColor: Colors.red,
+              child: const Icon(Icons.stop),
+            );
+          } else {
+            return FloatingActionButton(
+                child: const Icon(Icons.search), onPressed: () => FlutterBlue.instance.startScan(timeout: const Duration(seconds: 4)));
+          }
+        },
+      ),
+    );
+  }
+}
+
+// void main() async {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Bluethoot App',
+//       theme: ThemeData(
+//         // useMaterial3: true,
+//         primarySwatch: Colors.blue,
+//       ),
+//       // home: const ColorPickerScreen(),
+//       home: const MainPage(),
+//     );
+//   }
+// }
